9e95235811780af538f51d70f043cbc01c7983f1
Ahthony1388998 <suanaimous61051@163.com>
2025-08-26 18:04:31 +0800

    change6

--- test.cpp
+++ test.cpp
@@ -7,6 +7,6 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     vector<int> vec = {1, 2, 3, 4, 5};
-    cout<<vec[0];
+    cout<<vec[1];
     return 0;
 }
--- test2.cpp
+++ test2.cpp
@@ -6,5 +6,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     
+    cout<<"change3";
     return 0;
+    
 }
