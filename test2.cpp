8b012c2addabf26255002ab25634b2222524acc8
Ahthony1388998 <suanaimous61051@163.com>
2025-08-26 18:03:32 +0800

    change5

--- test2.cpp
+++ test2.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 
-
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    vector<int> vec = {1, 2, 3, 4, 5};
-    cout<<vec[0];
+    
     return 0;
 }
